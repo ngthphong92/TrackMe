3993fb99d4d51dfa24b03ba8c2c015ee1a462e31
Phong <ngthphong92@gmail.com>
2021-01-16 00:02:56 +0700

    + Add google maps api_key * Update gradle * Rename MainActivitym + Add user-permission

--- app/src/main/java/com/ngthphong92/trackme/MapsActivity.kt
+++ app/src/main/java/com/ngthphong92/trackme/MapsActivity.kt
@@ -3,7 +3,7 @@ package com.ngthphong92.trackme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
