a7a7bc86facc248da65f7c9b8c769b417b318099
Phong <ngthphong92@gmail.com>
2021-01-16 11:56:06 +0700

    * Rename MainActivity to MapActivity

--- app/src/main/java/com/ngthphong92/trackme/ui/MapsActivity.kt
+++ app/src/main/java/com/ngthphong92/trackme/ui/MapsActivity.kt
@@ -1,11 +1,12 @@
-package com.ngthphong92.trackme
+package com.ngthphong92.trackme.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ngthphong92.trackme.R
 
 class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_map)
     }
 }
